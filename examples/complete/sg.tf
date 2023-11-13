2b55ce74176fa3b0b893594fd45cdeb0eec0f8ff
john <jmwangi288@gmail.com>
2023-11-13 21:14:36 +0300

    fix: elb sg rules

--- examples/complete/sg.tf
+++ examples/complete/sg.tf
@@ -5,21 +5,28 @@ resource "aws_security_group" "elb" {
   tags        = local.tags
 }
 
+resource "aws_vpc_security_group_ingress_rule" "https" {
+  security_group_id = aws_security_group.elb.id
+  description       = "Rule to allow all http traffic to the elb"
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 443
+  to_port           = 443
+  ip_protocol       = "tcp"
+}
+
 resource "aws_vpc_security_group_ingress_rule" "http" {
   #checkov:skip=CKV_AWS_260:Ensure no security groups allow ingress from 0.0.0.0:0 to port 80
   security_group_id = aws_security_group.elb.id
   description       = "Rule to allow all http traffic to the elb"
   cidr_ipv4         = "0.0.0.0/0"
   from_port         = 80
-  ip_protocol       = "tcp"
   to_port           = 80
+  ip_protocol       = "tcp"
 }
 
 resource "aws_vpc_security_group_egress_rule" "egress" {
   security_group_id = aws_security_group.elb.id
   cidr_ipv4         = "0.0.0.0/0"
   description       = "Rule to allow all http traffic from the elb"
-  from_port         = 80
-  ip_protocol       = "tcp"
-  to_port           = 80
+  ip_protocol       = "-1"
 }
